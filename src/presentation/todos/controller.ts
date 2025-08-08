9511c867cecc569acaa9a14acc0f7f58d89ad7ec
Pedro <pedropefer34@gmail.com>
2025-08-08 09:54:10 +0200

    Implementamos enpoints que filtran por tareas completadas y no completadas

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -7,4 +7,33 @@ export class TodoController {
   public getTodos = (req: Request, res: Response) => {
     return res.json(todos);
   };
+
+  public getTodosById = (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    if (isNaN(id)) {
+      return res
+        .status(405)
+        .json({ error: `El argumento ID no es un número válido` });
+    }
+
+    const todoById = todos.find((todo) => todo.id === id);
+
+    todoById
+      ? res.json(todoById)
+      : res.status(404).json({ error: `Todo con ID ${id} no encontrado` });
+  };
+
+  public getTodoByCompleted = (req: Request, res: Response) => {
+    const completedTodos = todos.filter((todo) => todo.completado === true);
+    return res.json(completedTodos);
+  };
+
+  public getTodoByNotCompleted = (req: Request, res: Response) => {
+    const notCompletedTodos = todos.filter((todo) => todo.completado === false);
+    return res.json(notCompletedTodos);
+  };
+
+  public createTodo = (req: Request, res: Response) => {
+    return res.json(todos);
+  };
 }
--- src/presentation/todos/routes.ts
+++ src/presentation/todos/routes.ts
@@ -6,7 +6,12 @@ export class TodoRoutes {
     const router = Router();
     const todoController = new TodoController();
 
+    router.get("/completados", todoController.getTodoByCompleted);
+    router.get("/nocompletados", todoController.getTodoByNotCompleted);
+    router.get("/:id", todoController.getTodosById);
     router.get("/", todoController.getTodos);
+
+    router.post("/", todoController.createTodo);
 
     return router;
   }
